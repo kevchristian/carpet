76c423a6ebd7069ff08d0c06459fdb9170d342a5
kevchristian <kevinchristian086@gmail.com>
2024-01-24 10:31:58 +0700

    layout

---
+++ src/app/components/main.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <>
+            <div className="bg-[url('/images/group1.png')] w-full h-screen bg-no-repeat">
+            </div>
+        </>
+    )
+}
